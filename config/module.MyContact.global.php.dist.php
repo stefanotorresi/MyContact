1e677cbb88e03b81ffce87e601d43c49087ce950
Stefano Torresi <webdev@stefanotorresi.it>
2013-05-17 13:28:37 +0200

    add translator config dist file

---
+++ config/module.MyContact.global.php.dist.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * @author Stefano Torresi (http://stefanotorresi.it)
+ * @license See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace MyContact;
+
+return array(
+    'translator' => array(
+        'translation_file_patterns' => array(
+            array(
+                'type' => 'phpArray',
+                'base_dir'      => __DIR__ . '/../../../zendframework/zendframework/resources/languages',
+                'pattern'       => '%s/Zend_Captcha.php',
+                'text_domain'   => 'zend_captcha'
+            ),
+            array(
+                'type' => 'phpArray',
+                'base_dir'      => __DIR__ . '/../../../zendframework/zendframework/resources/languages',
+                'pattern'       => '%s/Zend_Validate.php',
+                'text_domain'   => 'zend_validate'
+            )
+        ),
+    ),
+);
