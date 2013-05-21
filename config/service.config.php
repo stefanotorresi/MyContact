8bf3339301f6c8f578f00b1ba23a93a9b9aab5d5
Stefano Torresi <webdev@stefanotorresi.it>
2013-05-21 19:20:21 +0200

    update service names to be more overridable

--- config/service.config.php
+++ config/service.config.php
@@ -4,10 +4,13 @@ namespace MyContact;
 
 return array(
     'factories' => array(
-        'translator'                => 'Zend\Mvc\Service\TranslatorServiceFactory',
-        'MyContactCaptcha'          => 'MyContact\Service\ContactCaptchaFactory',
-        'MyContactForm'             => 'MyContact\Service\ContactFormFactory',
-        'MyContactMailMessage'      => 'MyContact\Service\ContactMailMessageFactory',
-        'MyContactMailTransport'    => 'MyContact\Service\ContactMailTransportFactory',
+        'translator' => 'Zend\Mvc\Service\TranslatorServiceFactory',
+        'MyContactCaptcha' => 'MyContact\Service\ContactCaptchaFactory',
+        'MyContact\Form\ContactForm' => 'MyContact\Service\ContactFormFactory',
+        'MyContactMailMessage' => 'MyContact\Service\ContactMailMessageFactory',
+        'MyContactMailTransport' => 'MyContact\Service\ContactMailTransportFactory',
     ),
+    'aliases' => array(
+        'MyContactForm' => 'MyContact\Form\ContactForm',
+    )
 );
