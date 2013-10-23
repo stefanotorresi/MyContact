fa02f791c263585098f471fca2fa97355c64ae37
Stefano Torresi <webdev@stefanotorresi.it>
2013-10-23 12:58:15 +0200

    rename dist translator config

--- config/global.php.dist.php
+++ config/global.php.dist.php
@@ -13,13 +13,13 @@ return array(
         'translation_file_patterns' => array(
             array(
                 'type' => 'phpArray',
-                'base_dir'      => __DIR__ . '/../../../zendframework/zendframework/resources/languages',
+                'base_dir'      => 'vendor/zendframework/zendframework/resources/languages',
                 'pattern'       => '%s/Zend_Captcha.php',
                 'text_domain'   => 'zend_captcha'
             ),
             array(
                 'type' => 'phpArray',
-                'base_dir'      => __DIR__ . '/../../../zendframework/zendframework/resources/languages',
+                'base_dir'      => 'vendor/zendframework/zendframework/resources/languages',
                 'pattern'       => '%s/Zend_Validate.php',
                 'text_domain'   => 'zend_validate'
             )
