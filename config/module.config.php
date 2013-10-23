2e659f5cbdab3f783cddcc18553e244390de204a
Stefano Torresi <webdev@stefanotorresi.it>
2013-10-23 13:07:26 +0200

    add asset manager config

--- config/module.config.php
+++ config/module.config.php
@@ -86,4 +86,12 @@ return array(
             ),
         ),
     ),
+
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__ . '/../assets'
+            ),
+        ),
+    ),
 );
